6a87773315fd751d0c80a24a205c765da49d9693
Nicolas Gauthier <nick.habashi@gmail.com>
2025-06-15 13:49:14 -0400

    refactor: update webpack configuration to use CommonJS syntax and TypeScript entry point

--- webpack.common.js
+++ webpack.common.js
@@ -1,61 +1,62 @@
-import { resolve, join } from "path";
-import CopyPlugin from "copy-webpack-plugin";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { DefinePlugin } from "webpack";
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
-export const mode = "development";
-export const entry = {
-  app: "./src/index.js",
-};
-export const devtool = "eval-source-map";
-export const output = {
-  filename: "[name].js",
-  path: resolve(__dirname, "build"),
-};
-export const optimization = {
-  splitChunks: {
-    cacheGroups: {
-      commons: {
-        test: /[\\/]node_modules[\\/]/,
-        name: "vendor",
-        chunks: "all",
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.ts",
+  },
+  devtool: "eval-source-map",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "build"),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all",
+        },
       },
     },
   },
-};
-export const module = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-      },
-    },
-  ],
-};
-export const devServer = {
-  static: {
-    directory: join(__dirname, "build"),
-  },
-  compress: true,
-  port: 8080,
-};
-export const plugins = [
-  new DefinePlugin({
-    CANVAS_RENDERER: JSON.stringify(true),
-    WEBGL_RENDERER: JSON.stringify(true),
-  }),
-  new HtmlWebpackPlugin({
-    template: resolve(__dirname, "index.html"),
-    favicon: resolve(__dirname, "public/favicon.ico"),
-  }),
-  new CopyPlugin({
-    patterns: [
+  module: {
+    rules: [
       {
-        from: resolve(__dirname, "assets"),
-        to: resolve(__dirname, "build/assets"),
+        test: /\.ts?$/,
+        include: path.resolve(__dirname, "src"),
+        loader: "ts-loader",
       },
     ],
-  }),
-];
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "build"),
+    compress: true,
+    port: 8080,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      favicon: path.resolve(__dirname, "public/favicon.ico"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "assets"),
+          to: path.resolve(__dirname, "build/assets"),
+        },
+      ],
+    }),
+  ],
+};
